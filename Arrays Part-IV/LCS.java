57697962606aa613e87c8de1ca271ecd7ba8b3ad
Amit Anand <amitanand.asqre@gmail.com>
2026-01-01 22:35:35 +0530

    lcs code updated

--- Arrays Part-IV/LCS.java
+++ Arrays Part-IV/LCS.java
@@ -1,5 +1,14 @@
 /*
-method 	1 - Brute force,
+Method-1 Better 
+1. The question says consecutive, so first sort the array.
+2. Sorting brings all consecutive numbers next to each other.
+3. Traverse the array from left to right.
+4. Skip duplicate elements, as they don’t affect consecutiveness.
+5. If the difference with the previous element is 1, increase cnt.
+6. If the difference is not 1, update longest and reset cnt = 1.
+7. After traversal, update longest once more to include the last sequence.
+
+method 	2- optimal using set,
 1. Use a **HashSet** to store all elements of the array for **O(1)** lookup.
 2. Traverse the array element by element.
 3. If `num - 1` exists in the set, then `num` **cannot be the start** of a consecutive sequence → skip it.
@@ -12,24 +21,41 @@ method 	1 - Brute force,
 - **Time Complexity:** `O(n)`
 - **Space Complexity:** `O(n)`
 
-Method-2 Better 
-1. The question says consecutive, so first sort the array.
-2. Sorting brings all consecutive numbers next to each other.
-3. Traverse the array from left to right.
-4. Skip duplicate elements, as they don’t affect consecutiveness.
-5. If the difference with the previous element is 1, increase cnt.
-6. If the difference is not 1, update longest and reset cnt = 1.
-7. After traversal, update longest once more to include the last sequence.
-
 */
 
 class Solution {
     public int longestConsecutive(int[] nums) {
         int n = nums.length;
         if(n <= 1) return n;
-        // return solve2(nums, n);
-        return solve1(nums, n);
+        return solve2(nums, n);
+        // return solve1(nums, n);
     }
+
+    // optimal-using set
+    static int solve2(int[] nums, int n){
+        Set<Integer> set = new HashSet<>();
+
+        for(int i : nums){
+            set.add(i);
+        }
+
+        int longest = 0;
+
+        for (int num : set) {
+            if (set.contains(num - 1)) continue;
+
+            int cnt = 0;
+            int j = 0;
+            while (set.contains(num + j)) {
+                cnt++;
+                j++;
+            }
+            longest = Math.max(longest, cnt);
+        }
+
+        return longest;
+    }
+
 
     // Better 
     static int solve1(int[] nums, int n){
@@ -50,33 +76,5 @@ class Solution {
         }
 
         return Math.max(longest, cnt);
-    }
-
-        // Brute-using set
-    static int solve2(int[] nums, int n){
-        Set<Integer> set = new HashSet<>();
-
-        for(int i : nums){
-            set.add(i);
-        }
-
-        int longest = 0;
-
-        for(int num : nums){
-            // skip the element which is not starting of LCS.
-            if(set.contains(num - 1)){
-                continue;
-            }else{
-                int j  = 0;
-                int cnt = 0;
-                while(set.contains(num + j)){
-                    cnt++;
-                    j++;
-                }
-                longest = Math.max(longest, cnt);
-            }
-        }
-
-        return longest;
     }
 }
