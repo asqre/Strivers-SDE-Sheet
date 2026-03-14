a87abaaf3ab1560fb479e52beaf661ddfec517bf
Amit Anand <amitanand.asqre@gmail.com>
2026-03-14 22:31:32 +0530

    add the function

--- Recursion/3.cpp
+++ Recursion/3.cpp
@@ -8,10 +8,14 @@ public:
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
         int n = candidates.size();
         sort(candidates.begin(), candidates.end());
-        set<vector<int>> st;
         vector<int> temp;
-        solve1(candidates, n, 0, target, temp, st);
-        return {st.begin(), st.end()};
+        // set<vector<int>> st;
+        // solve1(candidates, n, 0, target, temp, st);
+        // return {st.begin(), st.end()};
+
+        vector<vector<int>> ans;
+        solve2(candidates, n, 0, target, temp, ans);
+        return ans;
     }
 private:
     void solve1(vector<int> &candidates, int n, int i, int target, vector<int> &temp, set<vector<int>> &st){
@@ -37,5 +41,26 @@ private:
         // not take
         solve1(candidates, n, i + 1, target, temp, st);
     }
+
+    void solve2(vector<int> &candidates, int n, int ind, int target, vector<int> &temp, vector<vector<int>> &ans){
+        if(target < 0){
+            return;
+        }
+
+        if(target == 0){
+            ans.push_back(temp);
+            return;
+        }
+
+        for(int i = ind; i < n; i++){
+            if(i > ind && candidates[i] == candidates[i - 1]) continue;
+
+            temp.push_back(candidates[i]);
+            solve2(candidates, n, i, target - candidates[i], temp, ans);
+            temp.pop_back();
+        }
+    }
 };
+
+
 
